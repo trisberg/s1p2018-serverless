487672a8086dad51a60a3d622f18beac0652d98f
trisberg <trisberg@pivotal.io>
2018-09-13 16:23:02 -0400

    Fixing app name

--- results-sink/src/test/java/com/example/resultssink/ResultsSinkAppTests.java
+++ results-sink/src/test/java/com/example/resultssink/ResultsSinkAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ResultsSinkTests {
+public class ResultsSinkAppTests {
 
 	@Test
 	public void contextLoads() {
